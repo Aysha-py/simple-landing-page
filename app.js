4b939260ecc4a1882db7882feba7b5e281a87881
Aysha Muhammed <m.aishat9890@gmail.com>
2021-12-20 08:02:33 -0800

    add

---
+++ app.js
@@ -0,0 +1,40 @@
+const price= [
+    {
+        category:"Stock Products",
+        Id:1,
+        range: "Varies",
+        image: "img/8.jpg",
+
+    },
+
+    {
+        category:"Luxury Products",
+        Id:2,
+        range: "Varies",
+        image: "img/6.jpg",
+  
+    },
+
+    {
+        category:"Sales Products",
+        Id:1,
+        range: 6000,
+        image: "img/5.jpg",
+
+    }
+]
+
+
+const displaypricing = price.map(function(item){
+    return (
+        `<div class="pricing">
+                <img src =${item.image} />
+                <h5 class="Price-title">${item.category}</h5>
+                <p class="price-range">${item.range}</p>
+            </div>
+        </div>`
+    )
+})
+
+const priceList =document.getElementById("price-grid");
+priceList.innerHTML= displaypricing.join("")
